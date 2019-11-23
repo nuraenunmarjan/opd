fd77fcfc3e4e1b9f2b1a8f7d9688e7f088d1b300
nuraenunmarjan <57859782+nuraenunmarjan@users.noreply.github.com>
2019-11-23 21:00:48 +0700

    Revert "Menambahkan Fungsi Detail pada Halaman Security Schedule"

--- application/config/routes.php
+++ application/config/routes.php
@@ -91,10 +91,5 @@ $route['employee/list']= 'C_Employee/add';
 $route['employee/edit/(:any)']= 'C_Employee/edit/$1';
 $route['employee/delete/(:any)']= 'C_Employee/delete/$1';
 
-//MANAGE SECURITY
-$route['security'] = 'C_Security/add';
-$route['security/detail/(:any)'] = 'C_Security/detail/$1';
-$route['security/list'] = 'C_Security/list';
-
 $route['404_override'] = 'C_Location/error';
 $route['translate_uri_dashes'] = FALSE;
